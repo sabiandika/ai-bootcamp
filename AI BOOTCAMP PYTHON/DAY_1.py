f3f305aaae1a193979f32d4898e359dc43ef9c6f
sabiandika <sabiandika2007@gmail.com>
2025-05-11 22:30:49 +0700

    Add_The_Output

--- AI BOOTCAMP PYTHON/DAY_1.py
+++ AI BOOTCAMP PYTHON/DAY_1.py
@@ -20,11 +20,17 @@ elif num == 0:
     print("Its a zero")
 else:
     print("Negatif Number:", num)
+    # outputnya positif number karena 10 lebih besar dari 0
+    # funsi if adalah untuk mengecek kondisi, jika kondisi benar maka akan dijalankan, jika tidak maka akan melompat ke selanjutnya
+    # fungsi elif adalah untuk mengecek kondisi lain, jika kondisi sebelumnya salah maka akan dijalankan
+    # fungsi else adalah untuk mengecek kondisi terakhir, jika kondisi sebelumnya salah maka akan dijalankan
 
 # dasar-dasar looping
 print("for")
+print("for")
 for i in range(8):
     print(i)
+    # outputnya 0,1,2,3,4,5,6,7 funsi for ini adalah untuk mengulang suatu aksi sebanyak n kali
 
 # while loop
 print("while")
@@ -32,6 +38,7 @@ count = 5
 while count > 0:
     print(count)
     count -= 1
+    # outputnya 5, 4, 3, 2, 1 fungsi while loop adalah untuk melakukan sesuatu sampai kondisi tertentu terpenuhi
 
 # break
 print("break")
@@ -39,6 +46,7 @@ for i in range(9):
     if i == 7:
         break  # sistem ngebaca program yang i=7 dan break untuk menghentikan looping
     print(i)
+    # outputnya 0,1,2,3,4,5,6 funsi break sendiri untuk memberhintikan looping
 
 # continue
 print("continue")
@@ -46,12 +54,14 @@ for i in range(7):  # looping 0-6
     if i == 4:
         continue  # sistem ngebaca program yang i=4 dan melompati i=4
     print(i)
+    # outputnya 0,1,2,3,5,6    4 tidak kekuar karena ada continue
 
 print("%")
 for i in range(7):
     if i % 2 == 0:
         continue  # sistem melompati 0, 2, 4 dan 6 namun tidak print 7 karna tidak ada modulo 7 dalam range
     print(i)
+    # outputnya 1, 3, 5, 6 karna 7 tidak ada dalam range dan
 
 
 # # mengecek bilangan prima
@@ -79,3 +89,4 @@ for nilai in angka:
         largest = nilai
 
 print("angka tebesar dalam list adalah: ", largest)
+# output : 97

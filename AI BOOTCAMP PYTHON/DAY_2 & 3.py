b90e0f2d1f86ce61e8b62fc1cb611a537cf8ace7
sabiandika <sabiandika2007@gmail.com>
2025-05-12 14:37:08 +0700

    DAY 2

---
+++ AI BOOTCAMP PYTHON/DAY_2 & 3.py
@@ -0,0 +1,24 @@
+# print("DAY 2")
+# # #SYNTAX FOR-LOOP
+# # for item in sequence:
+
+# print("Contoh for loop memakai list")
+# zoo = ["lion", "tiger", "bear"]
+# for animal in zoo:
+#     print(animal)
+
+# print("Contoh loop menggunakan range")
+# for a in range(8):
+#     print(a)
+
+# # #SYNTAX WHILE-LOOP
+# # while condition:
+
+# print("Contoh menggunakan while loop")
+# angka = 6
+# while angka > 0:
+#     angka -= 1
+#     print(angka)
+
+
+print("DAY 3")

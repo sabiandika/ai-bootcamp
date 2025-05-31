d95a40c4884fc9e92eed2dc29abd751205f7fdfe
sabiandika <sabiandika2007@gmail.com>
2025-05-31 14:34:06 +0700

    melengkapi

--- AI BOOTCAMP PYTHON/DAY_6_FILE_HANDLING.py
+++ AI BOOTCAMP PYTHON/DAY_6_FILE_HANDLING.py
@@ -14,3 +14,25 @@ def count_words_and_lines(filename): #Mendefinisikan sebuah fungsi bernama count
     print(f"File {filename} Not Found")
 
 count_words_and_lines("random.txt") #memanggil fungsi count.. dan file random.txt untuk input. Dan fungsi akan coba membaca dan menghitung baris dan kata dari file tsb.
+
+
+
+# Menulis dan membaca file
+def write_item_to_file(filename, items): #Mendefinisikan sebuah fungsi bernama write_to _file dengan parameter filename dan items, yaitu nama file dan isi file yang ingin ditulis.
+  with open(filename, "w") as file: #with agar file otomatis ditutup setelah digunakan. 
+    for item in items: #mengulang setiap item di dalam list items.
+      file.write (item + "\n") #menulis item ke dalam file, dengan pemisah baris baru ke bawah (\n) setiap item.
+
+def read__item_to_file(filename):
+  try: #ntuk menangani error, khususnya jika file tidak ditemukan. Kalau ada error, program akan loncat ke bagian except.
+    with open(filename, "r") as file:   #with agar file otomatis ditutup setelah digunakan ,file = nama
+      items = file.readlines()
+      print("Items in the file:")
+      for item in items:
+        print(item.strip()) #strip() untuk menghilangkan spasi di awal dan akhir setiap item. 
+  except FileNotFoundError:  # file yang error pada try akan terlempar ke except
+    print(f"File {filename} Not Found") 
+
+items = ["Apple", "Banana" "Cherry" "Date"]
+Write_item_to_file ("fruits.txt",)
+read_item_from_file ("fruits.txt")
--- AI BOOTCAMP PYTHON/DAY_7_PYTHONIC-CODE_PROJECK.py
+++ AI BOOTCAMP PYTHON/DAY_7_PYTHONIC-CODE_PROJECK.py
@@ -6,7 +6,7 @@ print(squares)
 
 #Filter angka genap
 pilter = [x for x in range(13) if x % 2 == 0]  #sama seperti yang diatas namun, pada if x akan di bagi dengan dua dan akan print bila sisa bagi x = 0
-print(pilter)                    #[x % 2 != 0] jika ingin angka ganjil
+print(pilter)                   #[x % 2 != 0] jika ingin angka ganjil
 
 #LAMBDA FUNCTION (Fungsi sangat sederhana(tidak diberi nama))
 #lambda argumen : expresi (sintaks)
@@ -19,11 +19,11 @@ print(kali(3,5))
 #map(function, iterable) (sintaks)
 numbers = [1, 2, 3, 4]
 squares = map(lambda x: x**2, numbers)
-# print(list(squares))
+print(list(squares))
 
 #filter() (Menyaring item dalam iterable berdasarkan kondisi (True/False) dari fungsi.)
 #filter(function, iterable) (sintaks)
-angka = [1, 2, 3, 4]
+angka = [1, 2, 3, 4,]
 even = filter(lambda x: x % 2 == 0, angka)
 print(list(even))
 
@@ -36,3 +36,17 @@ from functools import reduce
 numbers = [1, 2, 3, 4] # 1x2= 2, 2x3= 6, 6x4=24 
 product = reduce(lambda x,y: x * y, numbers)
 print(product) #ouput 24
+
+#OS DAN SYS MODUL
+#os menyediakan fungsi untuk berinteraksi dengan sistem operasi
+import os
+print(os.getcwd()) #untuk cek direktori saat ini
+#os.remove("nama_file") akan menghapus file
+#os.mkdir("nama_folder") akan membuat folder
+
+#sys menyediakan akses ke parameter dan fungsi spesifik sistem
+import sys
+print(sys.platform) #untuk cek sistem operasi yang digunakan.
+print(sys.argv[0]) #sys.argv adalah list (daftar) yang berisi argumen command-line. Elemen-elemen berikutnya (sys.argv[1], sys.argv[2], dst.) adalah argumen yang Anda tambahkan saat menjalankan skrip
+print(sys.version) #untuk cek versi python yang digunakan. 
+
